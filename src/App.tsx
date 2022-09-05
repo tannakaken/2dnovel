0cfb173e6db48b4fc108bb725ddfb125f8b15d28
Tanaka Kensaku <tannakaken@gmail.com>
2022-09-05 15:19:56 +0900

    縦横スワイプができる小説サイトの構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import "./App.css";
 import {
@@ -9,43 +9,122 @@ import {
 } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import Body from "./Body";
+import { useSpring, animated } from "@react-spring/web";
 
-function App() {
+const centerToCenter = 100;
+
+const App = () => {
+  const [i, setI] = useState(0);
+  const [j, setJ] = useState(0);
+  const [nextI, setNextI] = useState(0);
+  const [nextJ, setNextJ] = useState(0);
   const handlers = useSwipeable({
-    onSwipedDown: (eventData) => console.log("User Swiped Down!", eventData),
-    onSwipedUp: (eventData) => console.log("User Swiped Up!", eventData),
-    onSwipedRight: (eventData) => console.log("User Swiped Right!", eventData),
-    onSwipedLeft: (eventData) => console.log("User Swiped Left!", eventData),
+    onSwipedDown: () => setNextJ((j) => j + 1),
+    onSwipedUp: () => setNextJ((j) => j - 1),
+    onSwipedRight: () => setNextI((i) => i + 1),
+    onSwipedLeft: () => setNextI((i) => i - 1),
   });
+  const left = 50 + centerToCenter * (nextI - i);
+  const top = 50 + centerToCenter * (nextJ - j);
+  const mainStyles = useSpring({
+    left: `${left}%`,
+    top: `${top}%`,
+    onRest: () => {
+      setI(nextI);
+      setJ(nextJ);
+    },
+  });
+  const leftStyles = useSpring({
+    left: `${left - centerToCenter}%`,
+    top: "50%",
+  });
+  const rightStyles = useSpring({
+    left: `${left + centerToCenter}%`,
+    top: "50%",
+  });
+  const topStyles = useSpring({
+    left: "50%",
+    top: `${top - centerToCenter}%`,
+  });
+  const bottomStyles = useSpring({
+    left: "50%",
+    top: `${top + centerToCenter}%`,
+  });
+  const animating = i !== nextI || j !== nextJ;
+
   return (
     <div className="App">
-      <IconContext.Provider
-        value={{ color: "blue", className: "vertical-icon" }}
-      >
-        <AiOutlineUp />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "100%",
+      <IconContext.Provider value={{ className: "vertical-icon" }}>
+        <AiOutlineUp
+          color="red"
+          title="上へ戻る"
+          onClick={() => {
+            setNextJ(j - 1);
           }}
-        >
-          <IconContext.Provider
-            value={{ color: "blue", className: "horizontal-icon" }}
-          >
-            <AiOutlineLeft />
-            <div className="body" {...handlers}>
-              <Body />
-            </div>
-            <AiOutlineRight />
+        />
+        <div style={styles.centerRow}>
+          <IconContext.Provider value={{ className: "horizontal-icon" }}>
+            <AiOutlineLeft
+              color="red"
+              title="左へ戻る"
+              onClick={() => {
+                setNextI(i - 1);
+              }}
+            />
+            {animating ? (
+              <div>
+                <animated.div style={topStyles} className="body">
+                  <Body />
+                </animated.div>
+                <animated.div style={leftStyles} className="body">
+                  <Body />
+                </animated.div>
+                <animated.div style={mainStyles} className="body">
+                  <Body />
+                </animated.div>
+                <animated.div style={rightStyles} className="body">
+                  <Body />
+                </animated.div>
+                <animated.div style={bottomStyles} className="body">
+                  <Body />
+                </animated.div>
+              </div>
+            ) : (
+              <div>
+                <div className="body" {...handlers}>
+                  <Body />
+                </div>
+              </div>
+            )}
+            <AiOutlineRight
+              color="blue"
+              title="右へ進む"
+              onClick={() => {
+                setNextI(i + 1);
+              }}
+            />
           </IconContext.Provider>
         </div>
-        <AiOutlineDown />
+        <AiOutlineDown
+          color="blue"
+          title="下へ進む"
+          onClick={() => {
+            setNextJ(j + 1);
+          }}
+        />
       </IconContext.Provider>
     </div>
   );
-}
+};
+
+const styles = {
+  centerRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+} as const;
 
 export default App;
